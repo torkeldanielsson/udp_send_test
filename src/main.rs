a5ae0f92a870219ea6a4365d42bd4a79107c6863
torkel <torkel@voysys.se>
2020-01-21 19:19:44 +0100

    can send and receive

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,81 @@
-use std::net::UdpSocket;
-use std::thread;
-
-fn send_func()
-{
-    
-    loop {
-        thread::sleep(dur: 1);
-    }
-}
+use std::env;
+use std::net::{SocketAddr, UdpSocket};
+use std::time::{Duration, Instant};
 
 fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
 
-    thread::spawn(move || {
-        send_func()
-    });
+    let mut print_usage_instructions = args.len() != 3;
 
-    {
-        let mut socket = UdpSocket::bind("127.0.0.1:34254")?;
+    let send_interval_us = 1000;
+    let packet_size_bytes = 500;
 
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
+    if !print_usage_instructions {
+        let mode: &str = &args[1];
 
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-    } // the socket is closed here
+        match mode.as_ref() {
+            "tx" => {
+                let destination = &args[2];
+
+                println!("sending to {}", destination);
+
+                let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind to address");
+                socket.connect(destination).expect("connection failed");
+                let begin = Instant::now();
+                let mut next_action_time_ms = 1;
+
+                let mut buf: Vec<u8> = Vec::new();
+                buf.resize(packet_size_bytes, 0);
+
+                loop {
+                    while Instant::now().saturating_duration_since(begin)
+                        < Duration::from_millis(next_action_time_ms)
+                    {}
+
+                    next_action_time_ms += 1;
+
+                    socket.send(&buf)?;
+                }
+            }
+            "rx" => {
+                let listen_port = args[2]
+                    .parse::<u16>()
+                    .expect("Failed to parse destination port");
+
+                let socket = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], listen_port)))?;
+                socket
+                    .set_read_timeout(None)
+                    .expect("set_read_timeout call failed");
+
+                let mut buf = [0; 9000];
+
+                let mut last_rx_time = Instant::now();
+                let begin = Instant::now();
+                loop {
+                    let (number_of_bytes, src_addr) = socket.recv_from(&mut buf)?;
+
+                    let now = Instant::now();
+                    println!(
+                        "{:?};{:?};{};{}",
+                        now.saturating_duration_since(begin),
+                        now.saturating_duration_since(last_rx_time),
+                        number_of_bytes,
+                        src_addr
+                    );
+                    last_rx_time = now;
+                }
+            }
+            &_ => {
+                print_usage_instructions = true;
+            }
+        }
+    }
+
+    if print_usage_instructions {
+        println!(
+            "This program will either send a {} b udp packet every {} μs or listen for packets and print the time diff.
+To use, supply arguments: tx [target_ip:port] or: rx [listen_port]", packet_size_bytes, send_interval_us);
+    }
+
     Ok(())
 }
