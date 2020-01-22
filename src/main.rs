2fe71d19d1247461bc582f3f7818bcbd88b96455
torkel <torkel@voysys.se>
2020-01-22 19:09:06 +0100

    more gui wip

--- src/main.rs
+++ src/main.rs
@@ -23,71 +23,31 @@ struct State {
 
     quit: bool,
     fullscreen: bool,
-    line_draw_params: LineDrawParams,
 
-    link: Link,
-    up_link_settings: Arc<Mutex<LinkSettings>>,
-    down_link_settings: Arc<Mutex<LinkSettings>>,
-    sync_up_down: bool,
-
-    link_stats_update_time: Instant,
-
-    in_ports: Vec<u16>,
-    out_ports: Vec<u16>,
+    port: u16,
     address: String,
+}
+
+impl State {
+    fn new(port: u16, address: String) -> State {
+        State {
+            mouse_state: MouseState::new(),
+            last_mouse_state: MouseState::new(),
+            pan: [0.0, 0.0],
+            panning: false,
+            auto_pan: true,
+            scroll_factor: 2.2,
+            quit: false,
+            fullscreen: false,
+            port: port,
+            address: address,
+        }
+    }
 }
 
 fn draw_gui(ui: &Ui, state: &mut State) {
     let view_size = ui.io().display_size;
     let mut scale = f64::exp(state.scroll_factor as f64);
-
-    {
-        let elapsed = state.link_stats_update_time.reset();
-
-        let links = &mut [
-            (
-                state.link.get_up_simulator(),
-                &mut state.up_bandwidth_graph,
-                &mut state.up_dropped_graph,
-                &mut state.up_bytes_in_flight_graph,
-            ),
-            (
-                state.link.get_down_simulator(),
-                &mut state.down_bandwidth_graph,
-                &mut state.down_dropped_graph,
-                &mut state.down_bytes_in_flight_graph,
-            ),
-        ];
-
-        for (link_simulator, bandwidth_graph, dropped_graph, bytes_in_flight_graph) in links {
-            let stats = link_simulator.get_link_statistics();
-            {
-                // Update bandwidth
-
-                let bytes_sent_last_frame = stats.bytes_sent.swap(0, Ordering::SeqCst);
-                let bytes_per_sec =
-                    (8.0 * bytes_sent_last_frame as f64 / 1e6) / elapsed.as_secs_f64();
-
-                bandwidth_graph.push(bytes_per_sec);
-            }
-
-            {
-                // Update dropped
-
-                let bytes_dropped_last_frame = stats.bytes_dropped.swap(0, Ordering::SeqCst);
-                let bytes_dropped_per_sec =
-                    (8.0 * bytes_dropped_last_frame as f64 / 1e6) / elapsed.as_secs_f64();
-
-                dropped_graph.push(bytes_dropped_per_sec);
-            }
-
-            {
-                // Update Bytes In Flight
-
-                bytes_in_flight_graph.push(stats.bytes_in_flight.load(Ordering::SeqCst) as f64);
-            }
-        }
-    }
 
     Window::new(im_str!("Main"))
         .size([view_size[0] - 400.0, view_size[1]], Condition::Always)
@@ -149,194 +109,6 @@ fn draw_gui(ui: &Ui, state: &mut State) {
                     state.pan[0] += state.mouse_state.wheel.0 as f64;
                 }
             }
-
-            if state.auto_pan {
-                let last_pos = state.up_bandwidth_graph.get_last_x(view_size, scale);
-                state.pan[0] = last_pos as f64 - view_size[0] as f64 + 40.0;
-            }
-
-            {
-                let draw_list = ui.get_window_draw_list();
-
-                {
-                    let line_draw_params = &state.line_draw_params;
-
-                    let x1 = 0.0;
-                    let x2 = view_size[0];
-
-                    for i in 0..3 {
-                        for j in 0..=line_draw_params.lines[i] {
-                            let pos = transform_pos(
-                                [0.0, (-1.0 + (j as f32) * line_draw_params.line_spacing[i])],
-                                view_size,
-                                state.pan,
-                                1.0,
-                                scale,
-                            );
-
-                            draw_list
-                                .add_line([x1, pos[1]], [x2, pos[1]], line_draw_params.colors[i])
-                                .build();
-                        }
-                    }
-
-                    {
-                        // Draw bandwidth limit line
-                        let x1 = 0.0;
-                        let x2 = view_size[0];
-
-                        let pos = transform_pos(
-                            [
-                                0.0,
-                                state.up_link_settings.lock().unwrap().bandwidth_mbit
-                                    / state.up_bandwidth_graph.get_axis_max(),
-                            ],
-                            view_size,
-                            state.pan,
-                            1.0,
-                            scale,
-                        );
-
-                        draw_list
-                            .add_line([x1, pos[1]], [x2, pos[1]], [0.1, 0.8, 0.1, 1.0])
-                            .build();
-                    }
-                }
-
-                let pan = state.pan;
-                for graph in state.get_graphs() {
-                    graph.render(&draw_list, view_size, scale, pan);
-                }
-            }
-
-            let style_colors = ui.push_style_colors(&[
-                (
-                    StyleColor::Text,
-                    [0.980000019, 0.664439976, 0.303800017, 1.0],
-                ),
-                (
-                    StyleColor::CheckMark,
-                    [0.980000019, 0.664439976, 0.303800017, 1.0],
-                ),
-                (StyleColor::WindowBg, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::FrameBg, [1.0, 1.0, 1.0, 0.14]),
-                (StyleColor::SliderGrabActive, [0.9, 0.9, 0.9, 0.80]),
-                (StyleColor::PopupBg, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::ScrollbarBg, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::TitleBg, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::TitleBgActive, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::TitleBgCollapsed, [0.2, 0.2, 0.2, 1.0]),
-                (StyleColor::MenuBarBg, [0.165, 0.165, 0.165, 1.0]),
-                (StyleColor::Border, [0.314, 0.314, 0.314, 1.0]),
-                (StyleColor::BorderShadow, [0.0, 0.0, 0.0, 0.0]),
-                (StyleColor::SliderGrab, [0.6, 0.6, 0.6, 0.8]),
-                (StyleColor::SliderGrabActive, [0.9, 0.9, 0.9, 0.8]),
-                (StyleColor::ScrollbarGrab, [0.6, 0.6, 0.6, 0.8]),
-                (StyleColor::ScrollbarGrabActive, [0.9, 0.9, 0.9, 0.8]),
-                (StyleColor::ScrollbarGrabHovered, [0.6, 0.6, 0.6, 0.8]),
-                (StyleColor::Header, [0.15, 0.15, 0.15, 0.8]),
-                (StyleColor::HeaderActive, [0.4, 0.27, 0.13, 1.0]),
-                (StyleColor::HeaderHovered, [0.3, 0.2, 0.09, 1.0]),
-                (StyleColor::Button, [0.15, 0.15, 0.15, 0.8]),
-                (StyleColor::ButtonActive, [0.4, 0.27, 0.13, 1.0]),
-                (StyleColor::ButtonHovered, [0.3, 0.2, 0.09, 1.0]),
-                (StyleColor::FrameBgActive, [0.4, 0.27, 0.13, 1.0]),
-                (StyleColor::FrameBgHovered, [0.3, 0.2, 0.09, 1.0]),
-            ]);
-
-            Window::new(im_str!("Properties"))
-                .size([400.0, view_size[1]], Condition::Always)
-                .position([0.0, 0.0], Condition::Always)
-                .movable(false)
-                .resizable(false)
-                .title_bar(false)
-                .collapsible(false)
-                .focused(true)
-                .build(ui, || {
-                    {
-                        let mut up_link_settings = state.up_link_settings.lock().unwrap();
-                        let mut down_link_settings = state.down_link_settings.lock().unwrap();
-
-                        link_settings(
-                            ui,
-                            "Up Link Settings",
-                            &mut up_link_settings,
-                            &mut down_link_settings,
-                            &mut state.up_bytes_in_flight_graph,
-                            &mut state.down_bytes_in_flight_graph,
-                            state.sync_up_down,
-                            Some(state.link.get_up_from_port()),
-                        );
-
-                        link_settings(
-                            ui,
-                            "Down Link Settings",
-                            &mut down_link_settings,
-                            &mut up_link_settings,
-                            &mut state.down_bytes_in_flight_graph,
-                            &mut state.up_bytes_in_flight_graph,
-                            state.sync_up_down,
-                            None,
-                        );
-                    }
-
-                    ui.oden_checkbox(im_str!("Sync Up/Down"), &mut state.sync_up_down);
-                    ui.oden_checkbox(im_str!("Auto pan"), &mut state.auto_pan);
-
-                    if ui.oden_button(im_str!("Clear")) {
-                        for graph in state.get_graphs() {
-                            graph.reset();
-                        }
-                        state.auto_pan = true;
-                    }
-
-                    ui.tree_node(im_str!("Graphs")).build(|| {
-                        for graph in state.get_graphs() {
-                            graph.draw_gui(ui);
-                        }
-                    });
-
-                    ui.tree_node(im_str!("Internal")).build(|| {
-                        ui.oden_text(&im_str!(
-                            "Fps: {:.1} {:.2} ms",
-                            ui.io().framerate,
-                            1000.0 / ui.io().framerate
-                        ));
-
-                        ui.oden_text(&im_str!("In Ports: {:?}", &state.in_ports));
-                        ui.oden_text(&im_str!("Out Ports: {:?}", &state.out_ports));
-                        ui.oden_text(&im_str!("Address: {}", &state.address));
-
-                        ui.oden_text(&im_str!("Zoom {:?}", scale));
-
-                        ui.tree_node(im_str!("State")).build(|| {
-                            ui.oden_text(&im_str!("{:#?}", state));
-                        });
-
-                        ui.tree_node(im_str!("Style")).build(|| {
-                            ui.drag_int3(im_str!("Lines"), &mut state.line_draw_params.lines)
-                                .speed(0.01)
-                                .build();
-
-                            ui.drag_float3(
-                                im_str!("Line spacing"),
-                                &mut state.line_draw_params.line_spacing,
-                            )
-                            .speed(0.001)
-                            .build();
-
-                            for i in 0..3 {
-                                ColorEdit::new(
-                                    &im_str!("Color {}", i),
-                                    &mut state.line_draw_params.colors[i],
-                                )
-                                .build(ui);
-                            }
-                        });
-                    });
-                });
-
-            style_colors.pop(&ui);
         });
 }
 
@@ -412,13 +184,11 @@ fn main() -> Result<()> {
 
     let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
 
-    let mut last_mouse_state = MouseState::new();
-    let mut mouse_state = MouseState::new();
-    let mut quit = false;
+    let mut state = State::new(5005, "0.0.0.0".to_owned());
 
     loop {
-        last_mouse_state = mouse_state;
-        mouse_state.wheel = (0.0, 0.0);
+        state.last_mouse_state = state.mouse_state;
+        state.mouse_state.wheel = (0.0, 0.0);
 
         let mut new_absolute_mouse_pos = None;
 
@@ -432,19 +202,44 @@ fn main() -> Result<()> {
             match event {
                 Event::DeviceEvent { event, .. } => match event {
                     DeviceEvent::MouseMotion { delta: (x, y), .. } => {
-                        mouse_state.pos.0 += x / window.get_hidpi_factor();
-                        mouse_state.pos.1 += y / window.get_hidpi_factor();
+                        state.mouse_state.pos.0 += x / window.get_hidpi_factor();
+                        state.mouse_state.pos.1 += y / window.get_hidpi_factor();
                     }
                     _ => (),
                 },
                 Event::WindowEvent { event, .. } => match event {
                     WindowEvent::CloseRequested => {
-                        quit = true;
+                        state.quit = true;
                     }
                     WindowEvent::Resized(logical_size) => {
                         display
                             .gl_window()
                             .resize(logical_size.to_physical(window.get_hidpi_factor()));
+                    }
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        use glium::glutin::VirtualKeyCode as Key;
+
+                        let pressed = input.state == ElementState::Pressed;
+
+                        match input.virtual_keycode {
+                            Some(Key::Return) => {
+                                if pressed && input.modifiers.alt {
+                                    let monitor = if state.fullscreen {
+                                        None
+                                    } else {
+                                        Some(window.get_current_monitor())
+                                    };
+                                    window.set_fullscreen(monitor);
+                                    state.fullscreen = !state.fullscreen;
+                                }
+                            }
+                            Some(Key::Space) => {
+                                if pressed {
+                                    state.auto_pan = !state.auto_pan;
+                                }
+                            }
+                            _ => {}
+                        }
                     }
                     WindowEvent::CursorMoved {
                         position: LogicalPosition { x, y },
@@ -453,18 +248,18 @@ fn main() -> Result<()> {
                         new_absolute_mouse_pos = Some((x, y));
                     }
                     WindowEvent::MouseInput {
-                        state: winit_mouse_state,
+                        state: mouse_state,
                         button,
                         ..
                     } => match button {
                         MouseButton::Left => {
-                            mouse_state.pressed.0 = winit_mouse_state == ElementState::Pressed
+                            state.mouse_state.pressed.0 = mouse_state == ElementState::Pressed
                         }
                         MouseButton::Right => {
-                            mouse_state.pressed.1 = winit_mouse_state == ElementState::Pressed
+                            state.mouse_state.pressed.1 = mouse_state == ElementState::Pressed
                         }
                         MouseButton::Middle => {
-                            mouse_state.pressed.2 = winit_mouse_state == ElementState::Pressed
+                            state.mouse_state.pressed.2 = mouse_state == ElementState::Pressed
                         }
                         _ => {}
                     },
@@ -473,10 +268,10 @@ fn main() -> Result<()> {
                         ..
                     } => {
                         if x != 0.0 {
-                            mouse_state.wheel.0 = x as f32 * 50.0;
+                            state.mouse_state.wheel.0 = x as f32 * 50.0;
                         }
                         if y != 0.0 {
-                            mouse_state.wheel.1 = y as f32;
+                            state.mouse_state.wheel.1 = y as f32;
                         }
                     }
                     WindowEvent::MouseWheel {
@@ -484,10 +279,10 @@ fn main() -> Result<()> {
                         ..
                     } => {
                         if x != 0.0 {
-                            mouse_state.wheel.0 = x * 50.0;
+                            state.mouse_state.wheel.0 = x * 50.0;
                         }
                         if y != 0.0 {
-                            mouse_state.wheel.1 = y;
+                            state.mouse_state.wheel.1 = y;
                         }
                     }
                     _ => (),
@@ -496,24 +291,27 @@ fn main() -> Result<()> {
             }
         });
 
-        if !mouse_state.pressed.0 {
+        if !state.mouse_state.pressed.0 {
             if let Some(pos) = new_absolute_mouse_pos {
-                mouse_state.pos = pos;
+                state.mouse_state.pos = pos;
             }
         }
 
         {
-            imgui.io_mut().mouse_pos = [mouse_state.pos.0 as f32, mouse_state.pos.1 as f32];
+            imgui.io_mut().mouse_pos = [
+                state.mouse_state.pos.0 as f32,
+                state.mouse_state.pos.1 as f32,
+            ];
 
             imgui.io_mut().mouse_down = [
-                mouse_state.pressed.0,
-                mouse_state.pressed.1,
-                mouse_state.pressed.2,
+                state.mouse_state.pressed.0,
+                state.mouse_state.pressed.1,
+                state.mouse_state.pressed.2,
                 false,
                 false,
             ];
 
-            imgui.io_mut().mouse_wheel = mouse_state.wheel.1;
+            imgui.io_mut().mouse_wheel = state.mouse_state.wheel.1;
         }
 
         platform
@@ -523,8 +321,7 @@ fn main() -> Result<()> {
 
         {
             let ui = imgui.frame();
-
-            // draw_gui(&ui, &mut state);
+            draw_gui(&ui, &mut state);
 
             let mut target = display.draw();
             target.clear_color(0.12, 0.12, 0.12, 1.0);
@@ -534,7 +331,7 @@ fn main() -> Result<()> {
             target.finish().unwrap();
         }
 
-        if quit {
+        if state.quit {
             break;
         }
 
