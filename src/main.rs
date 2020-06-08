5baf9c8d616b6a8762ade2c34a5274525558d20e
Torkel <torkel@voysys.se>
2020-06-08 14:42:58 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::convert::TryInto;
 use std::env;
 use std::net::UdpSocket;
 use std::time::{Duration, Instant};
-use std::convert::TryInto;
+
+use rand::Rng;
 
 fn main() -> std::io::Result<()> {
+    let mut rng = rand::thread_rng();
+
     let args: Vec<String> = env::args().collect();
 
     let mut print_usage_instructions = args.len() != 3;
@@ -50,7 +54,6 @@ fn main() -> std::io::Result<()> {
                     let mut next_action_time_ms = 1;
 
                     let mut buf: Vec<u8> = Vec::new();
-                    buf.resize(packet_size_bytes, 0);
 
                     let mut packet_number: u64 = 0;
 
@@ -63,6 +66,8 @@ fn main() -> std::io::Result<()> {
                                 Instant::now().saturating_duration_since(begin).as_micros()
                             );
                         }
+
+                        buf.resize(rng.gen_range(10, 1400), 0);
 
                         while Instant::now().saturating_duration_since(begin)
                             < Duration::from_millis(next_action_time_ms)
@@ -104,32 +109,49 @@ fn main() -> std::io::Result<()> {
                     let mut buf = [0; 9000];
 
                     let mut last_rx_time = Instant::now();
-                    let begin = Instant::now();
-
 
                     let mut last_packet_nr: u64 = 0;
 
-                    loop {
-                        let (number_of_bytes, src_addr) = socket.recv_from(&mut buf)?;
+                    let mut average_time_between_rx_s = 0.0;
+                    let mut max_time_between_rx_s = 0.0;
+                    let mut acc_times = 0;
+                    let mut reorders = 0;
 
-                        if number_of_bytes == packet_size_bytes {
+                    loop {
+                        let (number_of_bytes, _src_addr) = socket.recv_from(&mut buf)?;
+
+                        if number_of_bytes > 8 {
                             let packet_nr = u64::from_le_bytes(buf[0..8].try_into().unwrap());
-                            
-                            if last_packet_nr + 1 != packet_nr {
-                                println!("reorder detected! ({} != {})",last_packet_nr + 1 , packet_nr);
+
+                            if last_packet_nr != 0 && last_packet_nr + 1 != packet_nr {
+                                reorders += 1;
                             }
                             last_packet_nr = packet_nr;
-                        }
 
-                        let now = Instant::now();
-                        //println!(
-                        //    "{};{};{};{}",
-                        //    now.saturating_duration_since(begin).as_nanos(),
-                        //    now.saturating_duration_since(last_rx_time).as_nanos(),
-                        //    number_of_bytes,
-                        //    src_addr
-                        //);
-                        last_rx_time = now;
+                            let now = Instant::now();
+
+                            let time_since_last_rx_s =
+                                now.saturating_duration_since(last_rx_time).as_secs_f64();
+
+                            average_time_between_rx_s += time_since_last_rx_s;
+                            if max_time_between_rx_s < time_since_last_rx_s {
+                                max_time_between_rx_s = time_since_last_rx_s;
+                            }
+
+                            acc_times += 1;
+
+                            if acc_times >= 10000 {
+                                average_time_between_rx_s /= 10.0;
+                                max_time_between_rx_s *= 1000.0;
+                                println!("Stats for last 10'000 samples: average time between rx: {} ms, max: {}, reorders: {}", average_time_between_rx_s, max_time_between_rx_s, reorders);
+
+                                acc_times = 0;
+                                max_time_between_rx_s = 0.0;
+                                acc_times = 0;
+                                reorders = 0;
+                            }
+                            last_rx_time = now;
+                        }
                     }
                 }
             }
